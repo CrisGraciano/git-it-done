32a36c5249dbd13f352f962ea2d0fe18fad9fcb4
CrisGraciano <cristobalgraciano98@gmail.com>
2022-08-22 19:57:44 -0500

    fetch set up

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,11 +1,13 @@
 var getUserRepos = function() {
-     var response = fetch("https://api.github.com/users/octocat/repos").then(function(response) {
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    // make request to the url
+    fetch(apiUrl).then(function(response) {
         response.json().then(function(data) {
             console.log(data);
         });
-     });
-
-     console.log("outside");
+    });
 };
 
 getUserRepos();
