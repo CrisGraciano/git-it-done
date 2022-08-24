291f17263ea3790245ccef41979602853a0e9ae2
CrisGraciano <cristobalgraciano98@gmail.com>
2022-08-23 23:15:44 -0500

    fixed error

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,6 @@
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
 var getUserRepos = function() {
     // format the github api url
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
